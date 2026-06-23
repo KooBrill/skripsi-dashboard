52cbda6676f23ef0aee4baae274aa6190ec7f6ea
Mui <164149216+KooBrill@users.noreply.github.com>
2026-06-23 11:31:41 +0700

    Update ThreatLog interface and add BannedLog interface

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,17 @@
 export interface ThreatLog {
   id: number
   ip_address: string
-  score: number
-  blocked_at: string
+  uri: string
+  user_agent: string
+  score_delta: number
+  total_score: number
+  is_tool: boolean
+  detected_at: string
+}
+
+export interface BannedLog {
+  id: number
+  ip_address: string
+  final_score: number
+  banned_at: string
 }
